50628faeb841e6a045992e52add950cada0f482c
Pinukio <gkswjddnjs15@gmail.com>
2024-01-06 15:38:34 +0900

    add Environment and Agent class

--- main.py
+++ main.py
@@ -2,3 +2,124 @@
 
 import random
 import numpy as np
+
+class GridWorld(): # SARSA와 동일
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.first_block = range(3)
+        self.second_block = range(2, 5)
+    
+    # Agent의 움직임을 나타냄
+    def step(self, a):
+        if a == 0:
+            self.move_left()
+        elif a == 1:
+            self.move_up()
+        elif a == 2:
+            self.move_right()
+        elif a == 3:
+            self.move_down()
+        
+        state = self.get_state()
+        reward = -1
+        done = self.is_done()
+
+        return state, reward, done
+    
+    # 회색인 부분으로는 갈 수 없음
+    def move_left(self):
+        if self.x == 0:
+            pass
+        elif self.x == 3 and self.y in self.first_block:
+            pass
+        elif self.x == 5 and self.y in self.second_block:
+            pass
+        else:
+            self.x -= 1
+    
+    def move_right(self):
+        if self.x == 6:
+            pass
+        elif self.x == 1 and self.y in self.first_block:
+            pass
+        elif self.x == 3 and self.y in self.second_block:
+            pass
+        else:
+            self.x += 1
+    
+    # y축 방향이 반대임
+    def move_up(self):
+        if self.y == 0:
+            pass
+        elif self.x == 2 and self.y == 3:
+            pass
+        else:
+            self.y -= 1
+    
+    def move_down(self):
+        if self.y == 4:
+            pass
+        elif self.x == 4 and self.y == 1:
+            pass
+        else:
+            self.y += 1
+
+    # 종료 State에 도달했는지 체크
+    def is_done(self):
+        if self.x == 6 and self.y == 4:
+            return True
+        else:
+            return False
+    
+    # 현재 Agent가 위치한 State를 반환
+    def get_state(self):
+        return (self.x, self.y)
+    
+    # 종료 State에 도달했을 때 리셋
+    def reset(self):
+        self.x = 0
+        self.y = 0
+
+class QAgent():
+    def __init__(self):
+        self.q_table = np.zeros((7, 5, 4))
+        # epsilon, decaying이므로 처음엔 큰 수를 넣음
+        self.eps = 0.9 
+
+    # decaying epsilon-greedy에 따라 Action을 결정함 (Behavior Policy)
+    def select_action(self, state): 
+        x, y = state
+        coin = random.random()
+        # 랜덤 Action을 선택하는 경우
+        if coin < self.eps: 
+            action = random.randint(0, 3)
+        else:
+            # 현 State의 Action Value를 모두 불러옴
+            action_val = self.q_table[x, y, :]
+            # Action Value 중 가장 높은 것을 선택
+            action = np.argmax(action_val)
+        return action
+    
+    def update_table(self, transition):
+        state, action, reward, state_prime = transition
+        x, y = state
+        x_prime, y_prime = state_prime
+        # Q-learning 업데이트 식을 이용함.
+        # SARSA와는 다르게 다음 Action을 선택하지 않음.
+        self.q_table[x, y, action] = self.q_table[x, y, action] + 0.1 * (reward + 1 * np.argmax(self.q_table[x_prime, y_prime, :]) - self.q_table[x, y, action])
+
+    def anneal_eps(self):
+        self.eps -= 0.01 # Q-learning에서는 천천히 줄어들도록 함
+        self.eps = max(self.eps, 0.2) # 최소 확률을 높임
+    
+    def show_table(self): # 각 State별 가장 높은 Action Value 출력
+        q_list = self.q_table.tolist()
+        data = np.zeros((7,5))
+        for row_idx in range(len(data)):
+            row = q_list[row_idx]
+            for col_idx in range(len(row)):
+                col = row[col_idx] # 각 State의 Action 4개
+                action = np.argmax(col)
+                data[row_idx, col_idx] = action
+        print(data.T)
